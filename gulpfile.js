9b02b73c3c92bd66cf68be5711e3bfcf42e25738
Victor Vianna <vitorvianna27@gmail.com>
2023-02-01 23:49:25 -0300

    fim do modulo 20 y.y

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,14 @@ const imagemin = require('gulp-imagemin')
 
 function tarefasCSS(cb) {
 
-    return gulp.src('./vendor/**/*.css')
-      .pipe(concat('libs.css'))
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './vendor/owl/css/owl.css',
+        './vendor/jquery-ui-1.13.2.custom/jquery-ui.css',
+        './src/css/style.css'
+    ])
+      .pipe(concat('styles.css'))
       .pipe(cssmin())
       .pipe(rename({ suffix: '.min'})) // para gerar o final do arquivo conforme o nome: """libs.min.css"""
       .pipe(gulp.dest('./dist/css'))
@@ -19,8 +25,17 @@ function tarefasCSS(cb) {
 
 function tarefasJS(){
 
-  return gulp.src('./vendor/**/*.js')
-      .pipe(concat('libs.js'))
+  return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/js/owl.js',
+        './vendor/jquery-mask/jquery.mask.js',
+        './vendor/jquery-ui-1.13.2.custom/jquery-ui.js',
+        './node_modules/@fortawesome/fontawesome-free/js/fontawesome.js',
+        './src/js/custom.js'
+
+    ])
+      .pipe(concat('scripts.js'))
       .pipe(uglify())
       .pipe(rename({ suffix: '.min'})) //libs.min.js
       .pipe(gulp.dest('./dist/js'))
